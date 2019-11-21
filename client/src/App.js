d3ca73544aa69f3fe699b1700dbd473834afc31a
unknown <devharris93@gmail.com>
2019-11-20 22:37:25 -0500

    Test iFrame added with style prop to main-page.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const styles = {
+  videoStyle: {
+    padding:"2px 0px 4px", 
+    display:"block", 
+    width:"345px", 
+    fontWeight:"normal", 
+    fontSize:"10px", 
+    textDecoration:"underline"
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +28,9 @@ function App() {
         >
           Learn React
         </a>
+        <br/>
+        <iframe id="video" src="https://player.twitch.tv/?autoplay=false&video=v503767319" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/videos/503767319?tt_content=text_link&tt_medium=vod_embed" style={styles.videoStyle}
+      ></a>
       </header>
     </div>
   );
