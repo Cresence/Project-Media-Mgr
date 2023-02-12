d68ce2b9778af2b6172afbd8990776864a176f54
Cresence <devharris93@gmail.com>
2023-02-11 21:53:16 -0500

    Re-added line to help with static assets per Render.com static site tutorial.

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 // Add routes, both API and view
 app.use(routes);
